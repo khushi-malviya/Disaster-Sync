0131a5aad1703bf887cc5d19878042d191b40938
khushi-malviya <khushimalviya11054@gmail.com>
2025-10-08 12:36:21 +0530

    Set up Express server and MongoDB Atlas connection

---
+++ server/app.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Atlas connection
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB Atlas'))
+.catch((err) => console.error('MongoDB connection error:', err));
+
+// Example route
+app.get('/', (req, res) => {
+  res.send('DisasterSync API is running!');
+});
+
+module.exports = app;
